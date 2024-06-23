3996716e572cd9bafcaf069634cc2db3b2998c3a
Ather06 <mdatherbiswas06@gmail.com>
2024-06-24 01:22:06 +0600

    route set up

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route set up
+Route::get('/profile/{id}', [ProfileController::class, 'index']);
